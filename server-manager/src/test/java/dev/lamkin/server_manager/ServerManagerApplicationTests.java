2d0aee4241ae1f0f169fed1768602f55d69d7601
Chase Lamkin <66813925+ENIACore@users.noreply.github.com>
2026-09-19 09:46:53 -0700

    [MAJOR][JAVA] Added initial java project template

---
+++ server-manager/src/test/java/dev/lamkin/server_manager/ServerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lamkin.server_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
