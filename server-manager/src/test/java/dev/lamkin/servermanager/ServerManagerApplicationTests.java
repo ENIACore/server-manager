99e7c7e59998720ed7f7a12ad39bbf1b74a638cd
Chase Lamkin <66813925+ENIACore@users.noreply.github.com>
2026-09-19 10:25:36 -0700

    [PATCH][JAVA] Added missing test rename

--- server-manager/src/test/java/dev/lamkin/servermanager/ServerManagerApplicationTests.java
+++ server-manager/src/test/java/dev/lamkin/servermanager/ServerManagerApplicationTests.java
@@ -1,4 +1,4 @@
-package dev.lamkin.server_manager;
+package dev.lamkin.servermanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ServerManagerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
