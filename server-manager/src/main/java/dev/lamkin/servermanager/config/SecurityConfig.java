7ec94f936fbddb49d714dec5b5d51a308004b672
Chase Lamkin <66813925+ENIACore@users.noreply.github.com>
2026-09-19 11:39:48 -0700

    [MINOR][JAVA] Added basic spring security setup

--- server-manager/src/main/java/dev/lamkin/servermanager/config/SecurityConfig.java
+++ server-manager/src/main/java/dev/lamkin/servermanager/config/SecurityConfig.java
@@ -1,5 +1,59 @@
 package dev.lamkin.servermanager.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/resources/**");
+    }
+
+    @Bean
+    public SecurityFilterChain SecurityFilterChain(HttpSecurity http) {
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/public/**").permitAll()
+                .anyRequest().hasRole(UserRoles.USER_ROLE));
+        http.formLogin(form -> form.loginPage("/login")
+                .successHandler((req, res, auth) -> res.setStatus(HttpServletResponse.SC_OK))
+                .failureHandler((req, res, auth) -> res.setStatus(HttpServletResponse.SC_FORBIDDEN)));
+        http.exceptionHandling(exceptionHandlingCustomizer -> exceptionHandlingCustomizer
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)));
+        http.csrf(null); // TODO
+
+        return http.build();
+
+    }
+
+    @Bean
+    public UserDetailsService UserDetailService(PasswordEncoder encoder) {
+        UserDetails user = User.withUsername("user")
+                .password(encoder.encode("password"))
+                .roles(UserRoles.USER_ROLE)
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
---
+++ server-manager/src/main/java/dev/lamkin/servermanager/config/UserRoles.java
@@ -0,0 +1,5 @@
+package dev.lamkin.servermanager.config;
+
+public final class UserRoles {
+    public final static String USER_ROLE = "USER";
+}
